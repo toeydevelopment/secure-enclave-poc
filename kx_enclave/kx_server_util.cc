77eb95bac68842bc869ef8ee81bcf8f4aa111da9
Anirut Kamchai <anirut.workspace@gmail.com>
2023-01-18 15:10:54 +0700

    :zap: add grpc secure enclave for testing

--- kx_enclave/kx_server_util.cc
+++ kx_enclave/kx_server_util.cc
@@ -13,11 +13,22 @@
 #include "asylo/util/status_macros.h"
 #include "kx_enclave/kx_enclave_grpc_server_config.pb.h"
 
-namespace kx {
-    namespace kx_enclave {
+namespace kx
+{
+    namespace kx_enclave
+    {
+
+        namespace
+        {
+
+            constexpr char kServerAddress[] = "localhost";
+            constexpr char kEnclaveName[] = "grpc_server";
+
+        } // namespace
         asylo::Status LoadGrpcServerEnclave(const std::string &enclave_path,
                                             const std::string &address,
-                                            int server_port, bool debug_enclave) {
+                                            int server_port, bool debug_enclave)
+        {
             // The EnclaveLoadConfig contains all configurations passed to the enclave for
             // initialization.
             asylo::EnclaveLoadConfig load_config;
@@ -32,7 +43,7 @@ namespace kx {
             // including the location of the enclave binary and whether to run in debug
             // mode.
             asylo::SgxLoadConfig *sgx_config =
-                    load_config.MutableExtension(asylo::sgx_load_config);
+                load_config.MutableExtension(asylo::sgx_load_config);
             sgx_config->mutable_file_enclave_config()->set_enclave_path(enclave_path);
             sgx_config->set_debug(debug_enclave);
 
@@ -44,12 +55,14 @@ namespace kx {
             return manager->LoadEnclave(load_config);
         }
 
-        asylo::StatusOr<int> GrpcServerEnclaveGetPort() {
+        asylo::StatusOr<int> GrpcServerEnclaveGetPort()
+        {
             asylo::EnclaveManager *manager = nullptr;
             ASYLO_ASSIGN_OR_RETURN(manager, asylo::EnclaveManager::Instance());
 
             asylo::EnclaveClient *client = manager->GetClient(kEnclaveName);
-            if (!client) {
+            if (!client)
+            {
                 return asylo::Status(asylo::error::FAILED_PRECONDITION,
                                      absl::StrCat(kEnclaveName, " not running"));
             }
@@ -57,19 +70,22 @@ namespace kx {
             asylo::EnclaveInput enclave_input;
             asylo::EnclaveOutput enclave_output;
             ASYLO_RETURN_IF_ERROR(client->EnterAndRun(enclave_input, &enclave_output));
-            if (!enclave_output.HasExtension(actual_server_port)) {
+            if (!enclave_output.HasExtension(actual_server_port))
+            {
                 return asylo::Status(asylo::error::INTERNAL,
                                      "Server output missing server_port extension");
             }
             return enclave_output.GetExtension(actual_server_port);
         }
 
-        asylo::StatusOr <string> GrpcServerEnclaveGetAddress() {
+        asylo::StatusOr<string> GrpcServerEnclaveGetAddress()
+        {
             asylo::EnclaveManager *manager = nullptr;
             ASYLO_ASSIGN_OR_RETURN(manager, asylo::EnclaveManager::Instance());
 
             asylo::EnclaveClient *client = manager->GetClient(kEnclaveName);
-            if (!client) {
+            if (!client)
+            {
                 return asylo::Status(asylo::error::FAILED_PRECONDITION,
                                      absl::StrCat(kEnclaveName, " not running"));
             }
@@ -77,19 +93,22 @@ namespace kx {
             asylo::EnclaveInput enclave_input;
             asylo::EnclaveOutput enclave_output;
             ASYLO_RETURN_IF_ERROR(client->EnterAndRun(enclave_input, &enclave_output));
-            if (!enclave_output.HasExtension(actual_server_address)) {
+            if (!enclave_output.HasExtension(actual_server_address))
+            {
                 return asylo::Status(asylo::error::INTERNAL,
                                      "Server output missing address extension");
             }
             return enclave_output.GetExtension(actual_server_address);
         }
 
-        asylo::Status DestroyGrpcServerEnclave() {
+        asylo::Status DestroyGrpcServerEnclave()
+        {
             asylo::EnclaveManager *manager = nullptr;
             ASYLO_ASSIGN_OR_RETURN(manager, asylo::EnclaveManager::Instance());
 
             asylo::EnclaveClient *client = manager->GetClient(kEnclaveName);
-            if (!client) {
+            if (!client)
+            {
                 return asylo::Status(asylo::error::FAILED_PRECONDITION,
                                      absl::StrCat(kEnclaveName, " not running"));
             }

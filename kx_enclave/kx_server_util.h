4fddc54a4d32c30ebc5f167aacde46ebf4c8e7e4
Anirut Kamchai <anirut.workspace@gmail.com>
2023-01-18 15:07:36 +0700

    :zap: add grpc secure enclave for testing

--- kx_enclave/kx_server_util.h
+++ kx_enclave/kx_server_util.h
@@ -5,23 +5,28 @@
 #ifndef ASYLO_EXAMPLES_KX_SERVER_UTIL_H
 #define ASYLO_EXAMPLES_KX_SERVER_UTIL_H
 
+#include <string>
 
-namespace kx {
-    namespace kx_enclave {
+#include "asylo/util/status.h"
+#include "asylo/util/statusor.h"
+
+namespace kx
+{
+    namespace kx_enclave
+    {
         asylo::Status
         LoadGrpcServerEnclave(const std::string &enclave_path, const std::string &address, int server_port,
                               bool debug_enclave);
 
-// Retrieves the port of the server inside the GrpcServerEnclave. Returns a
-// non-OK Status if the GrpcServerEnclave is not running.
+        // Retrieves the port of the server inside the GrpcServerEnclave. Returns a
+        // non-OK Status if the GrpcServerEnclave is not running.
         asylo::StatusOr<int> GrpcServerEnclaveGetPort();
 
         asylo::StatusOr<int> GrpcServerEnclaveGetAddress();
 
-
-// Destroys the GrpcServerEnclave and returns its finalization Status. Returns a
-// non-OK Status if the GrpcServerEnclave is not running.
+        // Destroys the GrpcServerEnclave and returns its finalization Status. Returns a
+        // non-OK Status if the GrpcServerEnclave is not running.
         asylo::Status DestroyGrpcServerEnclave();
     }
 }
-#endif //ASYLO_EXAMPLES_KX_SERVER_UTIL_H
+#endif // ASYLO_EXAMPLES_KX_SERVER_UTIL_H

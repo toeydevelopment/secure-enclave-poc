ac01caa22e53c4be0be043434b596708e4043af3
Anirut Kamchai <anirut.workspace@gmail.com>
2023-01-18 15:47:29 +0700

    :zap: scd make invoices public access

--- grpc_server/translator_server_impl.cc
+++ grpc_server/translator_server_impl.cc
@@ -22,56 +22,49 @@
 #include "absl/strings/str_cat.h"
 #include "include/grpcpp/grpcpp.h"
 
-namespace examples
-{
-  namespace grpc_server
-  {
+namespace examples {
+    namespace grpc_server {
 
-    TranslatorServerImpl::TranslatorServerImpl()
-        : Service(),
-          // Initialize the translation map with a few known translations.
-          translation_map_({{"asylo", "sanctuary"},
-                            {"istio", "sail"},
-                            {"kubernetes", "helmsman"}})
-    {
-    }
+        TranslatorServerImpl::TranslatorServerImpl()
+                : Service(),
+                // Initialize the translation map with a few known translations.
+                  translation_map_({{"asylo",      "sanctuary"},
+                                    {"istio",      "sail"},
+                                    {"kubernetes", "helmsman"}}) {
+        }
 
-    ::grpc::Status TranslatorServerImpl::GenerateKeyPair(
-        ::grpc::ServerContext *context, const GenerateKeyPairRequest *request,
-        GenerateKeyPairReply *response)
-    {
+        ::grpc::Status TranslatorServerImpl::GenerateKeyPair(
+                ::grpc::ServerContext *context, const GenerateKeyPairRequest *request,
+                GenerateKeyPairReply *response) {
 
-      // Return the translation.
-      response->set_encrypted_private_key("HELLO WORLD ENCRYPTED");
+            // Return the translation.
+            response->set_encrypted_private_key("HELLO WORLD ENCRYPTED");
 
-      return ::grpc::Status::OK;
-    }
+            return ::grpc::Status::OK;
+        }
 
-    ::grpc::Status TranslatorServerImpl::SignMessage(
-        ::grpc::ServerContext *context, const SignMessageRequest *request,
-        SignMessageReply *response)
-    {
-      // Confirm that |*request| has an |input_word| field.
-      if (!request->has_message())
-      {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "No message word given");
-      }
+        ::grpc::Status TranslatorServerImpl::SignMessage(
+                ::grpc::ServerContext *context, const SignMessageRequest *request,
+                SignMessageReply *response) {
+            // Confirm that |*request| has an |input_word| field.
+            if (!request->has_message()) {
+                return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+                                      "No message word given");
+            }
 
-      // Confirm that the translation map has a translation for the input word.
-      auto response_iterator =
-          translation_map_.find(absl::AsciiStrToLower(request->message()));
-      if (response_iterator == translation_map_.end())
-      {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              absl::StrCat("No known translation for \"",
-                                           request->message(), "\""));
-      }
+            // Confirm that the translation map has a translation for the input word.
+            auto response_iterator =
+                    translation_map_.find(absl::AsciiStrToLower(request->message()));
+            if (response_iterator == translation_map_.end()) {
+                return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+                                      absl::StrCat("No known translation for \"",
+                                                   request->message(), "\""));
+            }
 
-      // Return the translation.
-      response->set_signature(response_iterator->second);
-      return ::grpc::Status::OK;
-    }
+            // Return the translation.
+            response->set_signature(response_iterator->second);
+            return ::grpc::Status::OK;
+        }
 
-  } // namespace grpc_server
+    } // namespace grpc_server
 } // namespace examples
